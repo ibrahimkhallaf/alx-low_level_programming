d23b03547d33df4a0b1abaff40c4b02f6bd660c4
ibrahimkhallaf <ebrahimkhallaf4@gmail.com>
2023-03-21 16:19:40 -0700

    l

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,24 @@
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by anew line
+ * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
-	char ch;
+	char chr;
 	int i;
 
-	for (i = 1; i <= 10; i++)
-		for (ch = 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
-	_putchar('\n');
+	i = 0;
+
+	while (i < 10)
+	{
+		chr = 'a';
+		while (chr <= 'z')
+		{
+			_putchar(chr);
+			chr++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
