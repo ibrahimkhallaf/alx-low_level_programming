79ceeed5e64339742fefea7fa4ffbbb370a2c3e1
ibrahimkhallaf <ebrahimkhallaf4@gmail.com>
2023-03-28 16:02:08 -0700

    s1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ *swap_int -  swaps the values of two integers using to prameters
+ *
+ * @a: input prameter 1
+ * @b: input prameter 2
+ *
+ * RETURN: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
